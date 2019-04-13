81aee60b535c07b9ffc8407d8687acc2134f16a6
carulasan <cycarulasan@gmail.com>
2019-04-13 18:29:57 +0800

    view

---
+++ precache-manifest.b636e7d70e80ddbd0cdee5e415626441.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.c38ebd3c.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "fonts/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "a1beb459f2127d226861",
+    "url": "css/chunk-vendors.53cb8448.css"
+  },
+  {
+    "revision": "6536e4067e72c89cd3f1530d83e9888c",
+    "url": "fonts/mdi.6536e406.woff2"
+  },
+  {
+    "revision": "5d43a6f507961589ab369ccc6f6d301b",
+    "url": "fonts/mdi.5d43a6f5.woff"
+  },
+  {
+    "revision": "43d5c718339efeb1177aff722d72669d",
+    "url": "fonts/ionicons.43d5c718.woff2"
+  },
+  {
+    "revision": "a21b9e1aad96623ef91bca7ecea7b169",
+    "url": "fonts/ionicons.a21b9e1a.woff"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.8a9a261c.woff2"
+  },
+  {
+    "revision": "fc81a78e058dc9cc4ea5",
+    "url": "js/app.db1ecdf7.js"
+  },
+  {
+    "revision": "a1beb459f2127d226861",
+    "url": "js/chunk-vendors.2c52fba8.js"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "fonts/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "fonts/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "fonts/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "fonts/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "fonts/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "30eecfa18805f6ef73f23dea5f6a7f16",
+    "url": "index.html"
+  },
+  {
+    "revision": "fc81a78e058dc9cc4ea5",
+    "url": "css/app.132b61a9.css"
+  }
+];
